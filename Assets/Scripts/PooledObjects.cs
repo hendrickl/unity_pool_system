90450693b7375518436d34c5f586348415d7a0ea
hendrickl <65900996+hendrickl@users.noreply.github.com>
2023-02-06 00:01:40 +0100

    Add object pooling logic

--- Assets/Scripts/PooledObjects.cs
+++ Assets/Scripts/PooledObjects.cs
@@ -6,4 +6,21 @@ public class PooledObjects : MonoBehaviour
 {
     [SerializeField] private GameObject _objectToPool;
     private List<GameObject> _pool = new List<GameObject>();
+
+    private GameObject SpawnObjectFromPool(GameObject prefab, List<GameObject> pool, Vector3 location)
+    {
+        for (int i = 0; i < pool.Count; i++)
+        {
+            if (pool[i].activeInHierarchy == false)
+            {
+                pool[i].transform.position = location;
+                pool[i].gameObject.SetActive(true);
+                return pool[i];
+            }
+        }
+
+        GameObject objectToSpawn = Instantiate(prefab, location, Quaternion.identity);
+        pool.Add(objectToSpawn);
+        return objectToSpawn;
+    }
 }
